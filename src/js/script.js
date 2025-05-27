17feffb4f626dec4332ee011c4dcc2034ccf7805
Adesh Katiya <adeshkatiya27@gmail.com>
2025-05-28 00:51:33 +0530

    feat: add navigation menu overlay functionality
    added functions and event listeners for click event of open/close buttons of nav menu on mobile

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+const btnOpen = document.getElementById("nav-button-open");
+const btnClose = document.getElementById("nav-button-close");
+const navContent = document.querySelector(".nav__content");
+const navOverlay = document.querySelector(".nav__overlay");
+const media = window.matchMedia('( width < 69.375em )');
+
+function openMobileMenu() {
+    console.log("btn open");
+    btnOpen.setAttribute("aria-expanded", "true");
+}
+
+function closeMobileMenu() {
+    console.log("btn close");
+    btnOpen.setAttribute("aria-expanded", "false");
+}
+
+function setupNav(e) {
+    if (e.matches) {
+        console.log("is mobile");
+        setTimeout(() => {
+            navContent.style.display = "block";
+            navOverlay.style.display = "block";
+        }, 500);
+    } else {
+        console.log("is desktop");
+        navContent.style.display = "";
+    }
+}
+setupNav(media);
+
+btnOpen.addEventListener("click", openMobileMenu);
+
+btnClose.addEventListener("click", closeMobileMenu);
