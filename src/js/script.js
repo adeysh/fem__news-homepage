d8665fba92e699efa498e73162e603591e01b91a
Adesh Katiya <adeshkatiya27@gmail.com>
2025-05-28 02:01:48 +0530

    style: add accessibility features and keyboard navigation
    added keyboard navigation, added styles for keyboard focus

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body");
+const main = document.querySelector("main");
 const btnOpen = document.getElementById("nav-button-open");
 const btnClose = document.getElementById("nav-button-close");
 const navContent = document.querySelector(".nav__content");
@@ -9,24 +10,33 @@ function openMobileMenu() {
     console.log("btn open");
     btnOpen.setAttribute("aria-expanded", "true");
     body.classList.add("no-scroll");
+    navContent.removeAttribute("inert");
+    main.setAttribute("inert", "");
+    btnClose.focus();
 }
 
 function closeMobileMenu() {
     console.log("btn close");
     btnOpen.setAttribute("aria-expanded", "false");
     body.classList.remove("no-scroll");
+    navContent.setAttribute("inert", "");
+    main.removeAttribute("inert");
+    btnOpen.focus();
 }
 
 function setupNav(e) {
     if (e.matches) {
         console.log("is mobile");
+        navContent.setAttribute("inert", "");
         setTimeout(() => {
             navContent.style.display = "block";
             navOverlay.style.display = "block";
         }, 500);
     } else {
         console.log("is desktop");
+        navContent.removeAttribute("inert");
         navContent.style.display = "";
+        main.removeAttribute("inert");
     }
 }
 setupNav(media);
@@ -34,3 +44,10 @@ setupNav(media);
 btnOpen.addEventListener("click", openMobileMenu);
 
 btnClose.addEventListener("click", closeMobileMenu);
+
+// Accessibility
+document.addEventListener("keyup", (e) => {
+    if (e.key === "Tab") {
+        console.log(document.activeElement);
+    }
+});
